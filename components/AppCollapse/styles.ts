0c83adb16a1346b26ff44d829b12e5dbe9e338ea
Felipe Fontoura Lissa <felipe.lissa@hotmail.com>
2022-09-01 17:05:20 -0300

    taking off useless components

--- components/AppCollapse/styles.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const Container = styled.div`
-   display: flex;
-   justify-content: center;
-   box-shadow: 0 0 0.5vw ${({ theme }) => theme.black};
-   width: 80vw;
-`;
